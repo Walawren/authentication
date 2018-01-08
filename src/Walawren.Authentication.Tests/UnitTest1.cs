fb9af426e3f478e84843d249557afeaebe546705
Michael Carlson <Michael.Carlson2@extendhealth.com>
2018-01-08 12:19:45 -0700

    Created authentication and test project

---
+++ src/Walawren.Authentication.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Walawren.Authentication.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
