78747358056c64ec9f67abd0309808352dcb3e91
Michael Carlson <Michael.Carlson2@extendhealth.com>
2018-01-08 17:27:37 -0700

    Connect test client and Identity Server

---
+++ src/Walawren.Authentication.TestClient/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
+
+namespace Walawren.Authentication.TestClient
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            ClientCredentials().GetAwaiter().GetResult();
+            Console.ReadKey();
+        }
+
+        private static async Task ClientCredentials()
+        {
+            // discover endpoints from metadata
+            var disco = await DiscoveryClient.GetAsync("http://localhost:5000");
+            if (disco.IsError)
+            {
+                Console.WriteLine(disco.Error);
+                return;
+            }
+
+            // request token
+            var tokenClient = new TokenClient(disco.TokenEndpoint, "test-client", "secret");
+            var tokenResponse = await tokenClient.RequestClientCredentialsAsync("grappnel");
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return;
+            }
+
+            // call api
+            var client = new HttpClient();
+            client.SetBearerToken(tokenResponse.AccessToken);
+
+            var response = await client.GetAsync("http://localhost:5001/client-test");
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(response.StatusCode);
+                return;
+            }
+            else
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(JArray.Parse(content));
+            }
+        }
+
+        private static void ResourceOwnerCredentials()
+        {
+            
+        }
+    }
+}
---
+++ src/Walawren.Authentication/Config.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using IdentityServer4.Models;
+
+namespace Walawren.Authentication
+{
+    public class Config
+    {
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("grappnel", "Grappnel API")
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "grappnel-website",
+
+                    // no interactive user, use the clientid/secret for authentication
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
+
+                    // secret for authentication
+                    ClientSecrets =
+                    {
+                        new Secret("another secret".Sha256())
+                    },
+
+                    // scopes that client has access to
+                    AllowedScopes = {"grappnel"}
+                },
+                new Client
+                {
+                    ClientId = "test-client",
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPasswordAndClientCredentials,
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes = {"grappnel"}
+                }
+            };
+        }
+    }
+}
