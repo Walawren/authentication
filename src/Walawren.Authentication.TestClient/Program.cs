233661b552cf735c60989a2eb2f5e32ff9358bb0
Eric Carlson <e.carlson94@gmail.com>
2018-01-09 05:43:20 +0000

    Test Resource Owner

--- src/Walawren.Authentication.TestClient/Program.cs
+++ src/Walawren.Authentication.TestClient/Program.cs
@@ -8,24 +8,37 @@ namespace Walawren.Authentication.TestClient
 {
     class Program
     {
+        private static DiscoveryResponse _disco;
+        private static string _accessToken;
+
         public static void Main(string[] args)
         {
-            ClientCredentials().GetAwaiter().GetResult();
+            DiscoverEndpoints().GetAwaiter().GetResult();
+
+            Console.WriteLine("Use Client Credentials?");
+            var useClientCredentials = Console.ReadKey().KeyChar;
+            if(useClientCredentials == 'y')
+            {
+                ClientCredentials().GetAwaiter().GetResult();
+            }
+            else
+            {
+                ResourceOwnerCredentials().GetAwaiter().GetResult();
+            }
+
+            Console.WriteLine(_accessToken);
+            Console.WriteLine();
+
+            CallAPI().GetAwaiter().GetResult();
+
+            Console.WriteLine("Press any key to quit...");
             Console.ReadKey();
         }
 
         private static async Task ClientCredentials()
         {
-            // discover endpoints from metadata
-            var disco = await DiscoveryClient.GetAsync("http://localhost:5000");
-            if (disco.IsError)
-            {
-                Console.WriteLine(disco.Error);
-                return;
-            }
-
             // request token
-            var tokenClient = new TokenClient(disco.TokenEndpoint, "test-client", "secret");
+            var tokenClient = new TokenClient(_disco.TokenEndpoint, "test-client", "secret");
             var tokenResponse = await tokenClient.RequestClientCredentialsAsync("grappnel");
 
             if (tokenResponse.IsError)
@@ -34,9 +47,42 @@ namespace Walawren.Authentication.TestClient
                 return;
             }
 
+            _accessToken = tokenResponse.AccessToken;
+        }
+
+        private static async Task ResourceOwnerCredentials()
+        {
+            Console.WriteLine("Username:");
+            var userName = Console.ReadLine();
+            Console.WriteLine("Password:");
+            var password = Console.ReadLine();
+
+            var tokenClient = new TokenClient(_disco.TokenEndpoint, "test-client", "secret");
+            var tokenResponse = await tokenClient.RequestResourceOwnerPasswordAsync(userName, password, "grappnel");
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return;
+            }
+
+            _accessToken = tokenResponse.AccessToken;
+        }
+
+        private static async Task DiscoverEndpoints(){
+            // discover endpoints from metadata
+            _disco = await DiscoveryClient.GetAsync("http://localhost:5000");
+            if (_disco.IsError)
+            {
+                Console.WriteLine(_disco.Error);
+                return;
+            }
+        }
+
+        private static async Task CallAPI(){
             // call api
             var client = new HttpClient();
-            client.SetBearerToken(tokenResponse.AccessToken);
+            client.SetBearerToken(_accessToken);
 
 
             var url = "http://localhost:5001/api/identity";
@@ -52,11 +98,6 @@ namespace Walawren.Authentication.TestClient
                 var content = await response.Content.ReadAsStringAsync();
                 Console.WriteLine(JArray.Parse(content));
             }
-        }
-
-        private static void ResourceOwnerCredentials()
-        {
-            
         }
     }
 }
--- src/Walawren.Authentication/Config.cs
+++ src/Walawren.Authentication/Config.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using IdentityServer4.Models;
+using IdentityServer4.Test;
 
 namespace Walawren.Authentication
 {
@@ -42,6 +43,21 @@ namespace Walawren.Authentication
                         new Secret("secret".Sha256())
                     },
                     AllowedScopes = {"grappnel"}
+                }
+            };
+        }
+
+        public static List<TestUser> GetUsers(){
+            return new List<TestUser>{
+                new TestUser {
+                    SubjectId = "1",
+                    Username = "miccar",
+                    Password = "Password1"
+                },
+                new TestUser {
+                    SubjectId = "2",
+                    Username = "zacvel",
+                    Password = "Password2"
                 }
             };
         }
